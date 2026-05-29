17ef10ae86515739fd22f3b455b6cd7848016f7c
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-05-29 17:16:03 +0530

    "updated navbar"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,13 @@ import { BsRobot, BsCoin} from "react-icons/bs"
 import { HiOutlineLogout} from "react-icons/hi";
 import { FaUserAstronaut} from "react-icons/fa";
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 function Navbar() {
 
   const {userData} = useSelector((state)=>state.user)
   const [showCreditPopup,setShowCreditPopup] = useState(false)
   const [showUserPopup,setShowUserPopup] = useState(false)
+  const navigate = useNavigate()
 
   return (
     <div className='bg-[#f3f3f3] flex justify-center px-4 pt-6'>
@@ -28,22 +30,51 @@ function Navbar() {
         </div>
           <div className='flex items-center gap-6 relative'>
               <div className='relative'>
-                <button onClick={()=>setShowCreditPopup(!showCreditPopup)} className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
+                <button 
+                onClick={()=>{setShowCreditPopup
+                (!showCreditPopup);
+                setShowUserPopup(false)
+                }}
+                 className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
                     <BsCoin size={20}/>
                     {userData?.credit || 0}
                 </button>
                 {showCreditPopup && (
                   <div className='absolute right-[-50px] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50'>
                     <p className='text-sm text-gray-600 mb-4'>Need more credits to continue interview</p>
-                    <button ></button>
+                    
+                    <button
+                    onClick={()=>navigate("/pricing")}
+                    className='w-full bg-black text-white py-2 rounded-lg text-sm' >Buy more Credits</button>
                   </div>
                 )}
               </div>
+
+
               <div className='relative'>
-                <button className='w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font semibold'>
+                <button 
+                onClick={()=>{setShowUserPopup
+                  (!showUserPopup);
+                  setShowCreditPopup(false)
+                }}
+                className='w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font semibold'>
                    {userData ? userData?.name.slice(0,1).toUpperCase() :
                     <FaUserAstronaut size={16} />}
                 </button>
+
+                {showUserPopup && (
+                  <div className='absolute right-0 mt-3 w-48 bg-white shaadow-xl border borger-gray-200 rounded-xl p-4 z-50'>
+                    <p className='text-md text-blue-500 font-medium mb-1'>{userData?.name}</p>
+
+                    <button 
+                    onClick={()=>navigate("/history")}
+                    className='w-full text-left text-sm py-2 hower:text-black text-gray-600'>InterView History</button>
+                    <button className='w-full text-left text-sm py-2 flex items-center gap-2 text-red-500'>
+                      <HiOutlineLogout size={16}/>
+                      Logout</button>
+                  </div>
+
+                )}
               </div>
           </div>
          
