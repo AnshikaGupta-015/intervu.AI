ca2decb7f1bbba4fb8f1ace92f7d8a6479126d95
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-06-09 22:36:19 +0530

    updated footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
-export default function Footer() {
+import { BsRobot } from 'react-icons/bs'
+function Footer() {
   return (
     <div className='bg-[#f3f3f3] flex justify-center px-4 pb-10 py-4 pt-10'>
-      
+       <div className='w-full max-w-6xl bg-white rouned-[24px] shadow-sm boeder boeder-gray-200 py-8 px-3 text-center'>
+          <div className='flex justify-center items-center gap-3 mb-3'>
+            <div className='bg-black text-white p-2 rounded-lg'><BsRobot size={16}/></div>
+            <h2 className='font-semibold'>INTERVU.Ai</h2>
+          </div>
+            <p className='text-gray-500 text-sm max-w-xl mx-auto'>
+              AI-powered interview prepration platform designed to improve communication skills, technical depth and professional confidence.
+            </p>
+       </div>
     </div>
   )
 }
